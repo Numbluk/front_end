8e655312373b2e3d5a9ac03bd8e8127630120897
Numbluk <Lukas.nimmo@gmail.com>
2016-03-15 20:53:28 -0500

    Almost finished todos project

--- js/todos_project/script.js
+++ js/todos_project/script.js
@@ -15,9 +15,17 @@ $(document).ready(function() {
   } else {
     var todo_count = 0;
     var todos_by_month = {
-      "No Due Date": [],
-      completed: []
+      "No Due Date": []
     };
+  }
+
+  // Sort todos in todos_by_month by completed
+  function sortTodos() {
+    for (var date in todos_by_month) {
+      todos_by_month[date].sort(function(a, b) {
+        return a.finished - b.finished;
+      });
+    }
   }
 
   // Hamburger actions
@@ -56,7 +64,7 @@ $(document).ready(function() {
     if (month != "No Due Date") {
       // Add todo to a month if month exists
       // Otherwise add month first and then add todo
-      if (todos_by_month[month]) {
+      if (todos_by_month.hasOwnProperty(month)) {
         todos_by_month[month].push(todo);
       } else {
         todos_by_month[month] = [];
@@ -72,7 +80,6 @@ $(document).ready(function() {
   var getTodo = function(id_num) {
     for(var date in todos_by_month) {
       for(var i = 0; i < todos_by_month[date].length; i++) {
-        console.log(todos_by_month[date][i].id);
         if(todos_by_month[date][i].id  == id_num) {
           return todos_by_month[date][i];
         }
@@ -85,7 +92,6 @@ $(document).ready(function() {
   var removeTodo = function(id_num) {
     for(var date in todos_by_month) {
       for(var i = 0; i < todos_by_month[date].length; i++) {
-        console.log(todos_by_month[date][i].id);
         if(todos_by_month[date][i].id  == id_num) {
           var obj_deleted = todos_by_month[date].splice(i, 1);
           updateStorage();
@@ -96,20 +102,56 @@ $(document).ready(function() {
     return;
   };
 
+  // getUnfinishedByMonth: gets number of unfinished todos by month
+  function getUnfinishedByMonth(month) {
+    var count = 0;
+    var todos_list = todos_by_month[month];
+    for(var i = 0; i < todos_list.length; i++) {
+      if (todos_list[i].finished == false) { count++; }
+    }
+    return count;
+  }
+
+  // getAllUnfinished: returns total number of unfinished todos
+  function getAllUnfinished() {
+    var total = 0;
+    for(var month in todos_by_month) {
+      total += getUnfinishedByMonth(month);
+    }
+    return total;
+  }
+
   // Todos template
   var todo_list_source = $("#todo_list").html();
   var todo_list_template = Handlebars.compile(todo_list_source);
 
-  // Header template
+  // Template for list of dates that are unfinished
+  var unfinished_aside_template = Handlebars.compile($("#unfinished_aside").html());
 
-  function updatePane(todos_by_month) {}
+  Handlebars.registerHelper("getUnfinished", function(lists) {
+      return getUnfinishedByMonth(lists);
+  });
+
+  // Template for list of dates that are finished
+  var finished_aside_template = Handlebars.compile($("#finished_aside").html());
 
   function display(month) {
+    sortTodos();
+    var unfinished_dates = getUnfinishedDates();
+    $("ul#unfinished").html(unfinished_aside_template({date: unfinished_dates}));
+
+    var finished_dates = getFinishedDates();
+    $("ul#finished").html(finished_aside_template({date: finished_dates}));
+
+    $("h1 span#header_date").text(month);
+    $("h1 span#left_on_page").text(getUnfinishedByMonth(month));
+    $("span#all_todos_left").text(getAllUnfinished());
     $("ul.todos").html(todo_list_template({todos: todos_by_month[month]}));
   }
 
-    display("No Due Date");
+  display("No Due Date");
 
+  // Add a new todo. Default: "No Due Date"
   $("a#new_todo").on("click", function(event) {
     event.preventDefault();
     var temp_todo = new Todo(todo_count, "Title");
@@ -123,71 +165,73 @@ $(document).ready(function() {
     event.preventDefault();
     var $list_to_delete = $(this).closest("li");
     $list_to_delete.remove();
-    console.log(todos_by_month);
     removeTodo($list_to_delete.attr("id"));
-    console.log(todos_by_month);
   });
 
   // Toggle complete with checkbox
   $("ul.todos").on("click", "input", function(e) {
     e.stopPropagation();
-    console.log(this);
-    console.log($(this).closest("li").find("span"));
-    $(this)
-    $(this).closest("li").find("span").toggleClass("complete");
+    $target_todo = $(this).closest("li");
+    getTodo($target_todo.attr("id")).finished = true;
+    $target_todo.find("span").toggleClass("complete");
+    updateStorage();
+    display($target_todo[0].dataset.month);
   });
-
-  // getTodosByMonth: gets a group of todos by month
-  // var getTodosByMonth = function(month) {
-  //   return todos_by_month[month];
-  // };
-  //
-  // var getUnfinishedTodos = function() {
-  //
-  // };
-  //
-  // var getFinishedTodos = function() {
-  //
-  // };
 
   function toggleModal() {
     $("#modal").toggle();
     $("#modal_background").toggle();
   }
 
+  // Returns current month of the content from the header
+  function getCurrentMonthHeader() {
+    var text = $("h1 span#header_date").text();
+    return text;
+  }
+
   // Bring up modal on list name click
   $("ul.todos").on("click", "span.name", function(event) {
     toggleModal();
     var id = $(this).parent().attr("id");
+    var $current_span = $(this);
+    var $current_month = $current_span.closest("li")[0].dataset.month;
+    event.stopPropagation();
+    $("form").trigger("click", id)
 
     // Save information in modal to todo
-    $("#modal #save").on("click", function() {
+    $("form").on("click", "#save", function(event) {
+      event.stopImmediatePropagation();
       var data = $("form").serializeArray();
-      var todo = removeTodo(id);
-
+      var todo = removeTodo(id)[0];
+      console.log(todo);
       todo.title = data[0].value
 
       var month = data[2].value;
-      var year = data[3].value.substr(2,2);
+      var year = data[3].value;
       if(month && year) {
-        if (month.length == 1) { month = "0" + month; }
+        if (month.length == 1) { month = "0" + month; } // Date formatting
+        if (year.length == 4) { year = year.substr(2, 2); }
         todo.due_date = month + "/" + year;
       }
+
+      $current_span.closest("ul").after("<h4 id='update'>Update complete! Now due on: " + month + "/" + year + "</h4>");
 
       assignTodo(todo);
       $("#modal").hide();
       $("#modal_background").hide();
+      $("form").trigger("reset");
+      display(getCurrentMonthHeader());
     });
 
     // Mark todo complete
     $("#modal").on("click", "#mark_complete", function(e) {
       getTodo(id).finished = true;
-      display("No Due Date");
-
+      $current_span.closest("li").find("input").trigger("click");
       // Why does toggleModal() not bind the event here after the first todo
       // is marked as complete?
       $("#modal").hide();
       $("#modal_background").hide();
+      updateStorage();
     });
   });
 
@@ -196,20 +240,37 @@ $(document).ready(function() {
     toggleModal();
   });
 
+  // - getFinishedDates: returns array of dates whose todos are completely finished to put into template to display
+  function isDateFinished(date) {
+    var todo_list = todos_by_month[date];
+    for(var i = 0; i < todo_list.length; i++) {
+      if (todo_list[i].finished == false) { return false; }
+    }
+    return true;
+  }
 
-  // Milsetone 3:
-  // When a todo is given a date it should be congregated to the list on the left panel that relates to the given month -- DONE?
-  // Display:
-  // - list of all todos based on left-pane month/date
-  // - list of all unfinished months with todos in them (and number left)
-  // - list of all finished months with todos in them
-  // var unfinished_todos_source = $("#unfinished_todos_template").html();
-  // var unfinished_template = Handlebars.compile(unfinished_todos_source);
-  // var unfinished_html = unfinished_template(todos_by_month);
+  function getFinishedDates() {
+    var finished_dates = [];
+    for(var date in todos_by_month) {
+      if (isDateFinished(date)) { finished_dates.push(date); }
+    }
+    return finished_dates;
+  }
 
-  // var todos = ;
-  // $("ul.todos");
-  // $(".incomplete_todos ul");
-  // $(".completed_todos ul");
+  function getUnfinishedDates() {
+    var unfinished_dates = [];
+    for (var date in todos_by_month) {
+      if (!isDateFinished(date)) { unfinished_dates.push(date); }
+    }
+    return unfinished_dates;
+  }
 
+  $("ul#unfinished, ul#finished").on("click", "a", function(e) {
+    e.preventDefault();
+    //Grab date
+    var date = $(this).find("span.date").text();
+
+    // Display todos with templates
+    display(date);
+  });
 });
